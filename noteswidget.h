eaa1dc1c12ff2143b9f0b53f6ba149e82569d081
liu <2100012953@stu.pku.edu.cn>
2022-06-10 15:11:14 +0800

    add gitignore

---
+++ noteswidget.h
@@ -0,0 +1,18 @@
+﻿#ifndef NOTESWIDGET_H
+#define NOTESWIDGET_H
+
+#include <QWidget>
+#include<filterwidget.h>
+#include<datamanager.h>
+
+class NotesWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit NotesWidget(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // NOTESWIDGET_H
